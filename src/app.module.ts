129af7a9ad96e77b38f14b2b2cd8f8dea1229f1c
eomgerm <fishbread00@gmail.com>
2023-06-02 16:45:47 +0900

    users controller created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [],
