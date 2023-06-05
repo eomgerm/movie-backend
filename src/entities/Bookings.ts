adca627df2dffc374a79670708a574e886044282
eomgerm <fishbread00@gmail.com>
2023-06-05 23:10:38 +0900

    booked_at add default

--- src/entities/Bookings.ts
+++ src/entities/Bookings.ts
@@ -1,49 +1,53 @@
-import { Column, Entity, Index, JoinColumn, ManyToOne } from "typeorm";
-import { ScreenSchedule } from "./ScreenSchedule";
-import { Users } from "./Users";
+import { Column, Entity, Index, JoinColumn, ManyToOne } from 'typeorm';
+import { ScreenSchedule } from './ScreenSchedule';
+import { Users } from './Users';
 
 @Index(
-  "FK_bookings_schedule_id_screen_schedule_schedule_id",
-  ["scheduleId"],
-  {}
+  'FK_bookings_schedule_id_screen_schedule_schedule_id',
+  ['scheduleId'],
+  {},
 )
-@Index("IX_bookings_1", ["userId"], {})
-@Entity("bookings", { schema: "movie" })
+@Index('IX_bookings_1', ['userId'], {})
+@Entity('bookings', { schema: 'movie' })
 export class Bookings {
-  @Column("bigint", {
+  @Column('bigint', {
     primary: true,
-    name: "booking_id",
-    comment: "예약 아이디",
+    name: 'booking_id',
+    comment: '예약 아이디',
   })
   bookingId: string;
 
-  @Column("datetime", { name: "booked_at", comment: "예매 시간" })
+  @Column('datetime', {
+    name: 'booked_at',
+    comment: '예매 시간',
+    default: () => "'CURRENT_TIMESTAMP(6)'",
+  })
   bookedAt: Date;
 
-  @Column("bigint", { name: "schedule_id", comment: "예약한 영화" })
+  @Column('bigint', { name: 'schedule_id', comment: '예약한 영화' })
   scheduleId: string;
 
-  @Column("bigint", { name: "user_id", comment: "예약한 사용자" })
+  @Column('bigint', { name: 'user_id', comment: '예약한 사용자' })
   userId: string;
 
-  @Column("int", { name: "status", comment: "상태" })
+  @Column('int', { name: 'status', comment: '상태' })
   status: number;
 
-  @Column("varchar", { name: "seats", comment: "예약한 자리", length: 30 })
+  @Column('varchar', { name: 'seats', comment: '예약한 자리', length: 30 })
   seats: string;
 
   @ManyToOne(
     () => ScreenSchedule,
     (screenSchedule) => screenSchedule.bookings,
-    { onDelete: "RESTRICT", onUpdate: "RESTRICT" }
+    { onDelete: 'RESTRICT', onUpdate: 'RESTRICT' },
   )
-  @JoinColumn([{ name: "schedule_id", referencedColumnName: "scheduleId" }])
+  @JoinColumn([{ name: 'schedule_id', referencedColumnName: 'scheduleId' }])
   schedule: ScreenSchedule;
 
   @ManyToOne(() => Users, (users) => users.bookings, {
-    onDelete: "CASCADE",
-    onUpdate: "RESTRICT",
+    onDelete: 'CASCADE',
+    onUpdate: 'RESTRICT',
   })
-  @JoinColumn([{ name: "user_id", referencedColumnName: "userId" }])
+  @JoinColumn([{ name: 'user_id', referencedColumnName: 'userId' }])
   user: Users;
 }
