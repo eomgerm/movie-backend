2e7cc0e4e48a38f7936e46ce3834c4cbf59ba0fa
eomgerm <fishbread00@gmail.com>
2023-06-06 01:10:58 +0900

    refactor

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MovieController } from './movie.controller';
 import { MovieService } from './movie.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Movies } from 'src/entities/Movies';
 
 @Module({
   imports: [],
