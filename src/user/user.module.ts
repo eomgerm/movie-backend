3469ee5ba10d2e837e4b7b91b455f0158dde9174
eomgerm <fishbread00@gmail.com>
2023-06-05 23:11:12 +0900

    refactoring

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { Users } from 'src/entities/Users';
 
 @Module({
   controllers: [UserController],
